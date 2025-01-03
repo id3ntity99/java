e77a41d92adf74dfed885181cc2894e467e4e6a8
Jason Lee <jas06113@gmail.com>
2025-01-03 15:34:40 +0900

    add ch01

---
+++ ch01/src/ch01/Hello.java
@@ -0,0 +1,12 @@
+package ch01;
+
+/**
+ * 날짜: 2024/12/31
+ * 이름: 이현민
+ * 내용: ch.01.Java 개발환경 구축
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ ch01/src/ch01/PrintTest.java
@@ -0,0 +1,25 @@
+package ch01;
+
+/*
+ * 날짜: 2024/12/31
+ * 이름: 이현민
+ * 내용: 출력 테스트
+ */
+public class PrintTest {
+	public static void main(String[] args) {
+		//기본출력
+		System.out.println("Hello java!");
+		System.out.println("Hello Korea!");
+		System.out.println("Hello Busan!");
+
+		//한줄출력
+		System.out.print("Welcome");
+		System.out.print(" Java");
+		System.out.print(" Spring!");
+
+		//개행출력
+		System.out.println();
+		System.out.print("Greeting!\n");
+		System.out.print("Java");
+	}
+}
