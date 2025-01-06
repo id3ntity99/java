9e81f6f58c684effccb38285f05caa0ca1714b2a
id3ntity99 <jas06113@gmail.com>
2025-01-06 17:25:08 +0900

    Completed test4

--- CodingTest/src/step3/Test04.java
+++ CodingTest/src/step3/Test04.java
@@ -1,7 +1,24 @@
 package step3;
 
+import java.util.Scanner;
+
 public class Test04 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int total = sc.nextInt();
+    int types = sc.nextInt();
 
+    int sum = 0;
+    for (int i = 0; i < types; i++) {
+      int price = sc.nextInt();
+      int number = sc.nextInt();
+      sum += price * number;
+    }
+    sc.close();
+
+    if (sum == total)
+      System.out.println("Yes");
+    else
+      System.out.println("No");
   }
 }
