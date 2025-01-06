afce63475a58dc81bc13ed951c849d5f6dc2bd0d
id3ntity99 <jas06113@gmail.com>
2025-01-06 17:36:07 +0900

    Created a java file for test12

---
+++ CodingTest/src/step3/Test12.java
@@ -0,0 +1,15 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Test12 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int sum = 0;
+    while (scanner.hasNextInt()) {
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
+    }
+    scanner.close();
+  }
+}
