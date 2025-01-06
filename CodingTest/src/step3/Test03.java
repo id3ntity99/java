0749e6cc0215d28016850d19b86469d8af983846
id3ntity99 <jas06113@gmail.com>
2025-01-06 13:59:17 +0900

    Completed test3

--- CodingTest/src/step3/Test03.java
+++ CodingTest/src/step3/Test03.java
@@ -1,5 +1,18 @@
 package step3;
 
-public class Test03 {
+import java.util.Scanner;
 
+public class Test03 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int max = sc.nextInt();
+    sc.close();
+
+    int sum = 0;
+    for (int i = 1; i <= max; i++) {
+      sum += i;
+    }
+
+    System.out.println(sum);
+  }
 }
