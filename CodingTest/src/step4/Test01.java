92c59a00396571d5c48d97101a96f8be19bc8850
id3ntity99 <jas06113@gmail.com>
2025-01-07 17:42:39 +0900

    Solved test1

--- CodingTest/src/step4/Test01.java
+++ CodingTest/src/step4/Test01.java
@@ -1,5 +1,23 @@
 package step4;
 
-public class Test01 {
+import java.util.Scanner;
 
+public class Test01 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] buf = new int[n];
+    for (int i = 0; i < n; i++) {
+      buf[i] = sc.nextInt();
+    }
+    int target = sc.nextInt();
+    sc.close();
+
+    int cnt = 0;
+    for (int j = 0; j < n; j++) {
+      if (buf[j] == target)
+        cnt++;
+    }
+    System.out.println(cnt);
+  }
 }
