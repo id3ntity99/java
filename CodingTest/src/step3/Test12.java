141fbdb001c4338797dd362f31cb09dc407d7701
id3ntity99 <jas06113@gmail.com>
2025-01-07 17:31:46 +0900

    Solved test12!

--- CodingTest/src/step3/Test12.java
+++ CodingTest/src/step3/Test12.java
@@ -1,15 +1,30 @@
 package step3;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Test12 {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int sum = 0;
-    while (scanner.hasNextInt()) {
-      int x = scanner.nextInt();
-      int y = scanner.nextInt();
+  public static void main(String[] args) throws IOException {
+    InputStreamReader in = new InputStreamReader(System.in);
+    OutputStreamWriter out = new OutputStreamWriter(System.out);
+    BufferedReader reader = new BufferedReader(in);
+    BufferedWriter writer = new BufferedWriter(out);
+
+    while (true) {
+      String s = reader.readLine();
+      if (s == null)
+        break;
+      String[] numbers = s.split(" ");
+      int x = Integer.parseInt(numbers[0]);
+      int y = Integer.parseInt(numbers[1]);
+      writer.write(String.valueOf(x + y));
+      writer.newLine();
     }
-    scanner.close();
+    writer.flush();
+    reader.close();
+    writer.close();
   }
 }
