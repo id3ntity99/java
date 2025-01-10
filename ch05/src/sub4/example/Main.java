73a8c340af97af0da6b332b53dbf6fce9e183cc8
id3ntity99 <jas06113@gmail.com>
2025-01-10 10:32:23 +0900

    Refactored getting random operation

--- ch05/src/sub4/example/Main.java
+++ ch05/src/sub4/example/Main.java
@@ -1,5 +1,7 @@
 package sub4.example;
 
+import java.util.Random;
+
 public class Main {
   public static void main(String[] args) {
     // calc 변수의 타입이 RollingChecksumCalc abstract class임에 주의할 것.
@@ -8,7 +10,7 @@ public class Main {
     System.out.println(hash);
     System.out.println(calc.toHex().toUpperCase());
 
-    int r = (int) Math.floor(Math.random() * Integer.MAX_VALUE);
+    int r = (int) Math.floor(new Random().nextInt() * Integer.MAX_VALUE);
     System.out.println(r);
   }
 }
