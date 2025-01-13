4f99a39afbe11109aac264a22c7b5d52bf2293ed
id3ntity99 <jas06113@gmail.com>
2025-01-13 17:46:06 +0900

    Java test 3

---
+++ JavaTest/src/test3/Test02.java
@@ -0,0 +1,25 @@
+package test3;
+
+class King {
+  private String name;
+  private int year;
+
+  public King(String name, int year) {
+    this.name = name;
+    this.year = year;
+  }
+
+  public King(int year) {
+    this("정조");
+    this.year = year;
+  }
+
+  public King(String name) {
+
+  }
+
+}
+
+
+public class Test02 {
+}
