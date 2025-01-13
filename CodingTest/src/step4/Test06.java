275818f592ce15c79ab242f1c7efd539485231ee
Jason Lee <jas06113@gmail.com>
2025-01-14 00:53:35 +0900

    Solved test6

--- CodingTest/src/step4/Test06.java
+++ CodingTest/src/step4/Test06.java
@@ -2,10 +2,35 @@ package step4;
 
 import java.util.Scanner;
 
+/*-
+ * - BOJ 10813
+ * - Swapping two elements of an integer array
+ */
 public class Test06 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int m = sc.nextInt();
+    int[] buf = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      buf[i] = i + 1;
+    }
+
+    for (int j = 0; j < m; j++) {
+      int src = sc.nextInt() - 1;
+      int dest = sc.nextInt() - 1;
+      int tmp = 0;
+
+      tmp = buf[dest];
+      buf[dest] = buf[src];
+      buf[src] = tmp;
+
+    }
+    sc.close();
+
+    for (int k = 0; k < n; k++) {
+      System.out.print(buf[k] + " ");
+    }
   }
 }
