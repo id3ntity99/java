58575ca8f8869483fa502cd46b4c06a153ea514f
id3ntity99 <jas06113@gmail.com>
2025-01-13 17:46:48 +0900

    solving test 7 using insertion sort and binary search

--- CodingTest/src/step4/Test07.java
+++ CodingTest/src/step4/Test07.java
@@ -3,37 +3,70 @@ package step4;
 import java.util.Scanner;
 
 public class Test07 {
-  private static final int STUDENT_NUM = 30;
-  private static final int SUBMITTED_STUDENT_NUM = 28;
-
-  public static void main(String[] args) {
-    int[] buf = new int[SUBMITTED_STUDENT_NUM];
-
-    // Initialize a buffer of total student numbers.
-    for (int i = 1; i <= STUDENT_NUM; i++) {
-      buf[i] = i;
-    }
-
-    // Do binary search
-    Scanner sc = new Scanner(System.in);
-    int start, end = 0;
-    int min, minNext = 0;
-    int mid = buf.length / 2;
-
-    for (int i = 0; i < SUBMITTED_STUDENT_NUM; i++) {
-      int key = sc.nextInt();
-      if (key > buf[mid]) {
-        // Choose right-half of the array.
-        start = 0;
+  private static boolean search(int key, int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    int mid = (start + end) / 2;
+    while (start <= end) {
+      if (key == arr[mid])
+        return true;
+      else if (key < arr[mid]) {
+        // Select left-half
         end = mid - 1;
-        mid = (int) Math.ceil(end - start + 1);
-      } else if (key < buf[mid]) {
-        // Choose left-half of the array.
-      } else {
-        continue;
+        mid = (start + end) / 2;
+      } else if (key > arr[mid]) {
+        // Select right-half
+        start = mid + 1;
+        mid = (start + end) / 2;
       }
     }
+    return false;
+  }
+
+  private static int[] initArray(int len) {
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++) {
+      arr[i] = i + 1;
+    }
+    return arr;
+  }
+
+  private static int[] initArray(int len, Scanner sc) {
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++) {
+      arr[i] = sc.nextInt();
+    }
     sc.close();
-    System.out.println(min + " " + minNext);
+    return arr;
+  }
+
+  private static void sort(int[] buf) {
+    for (int i = 0; i < buf.length - 1; i++) {
+      int key = buf[i + 1];
+      int j = i;
+      while (j >= 0 && buf[j] > key) {
+        int tmp = buf[j];
+        buf[j] = key;
+        buf[j + 1] = tmp;
+        j--;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] arr = initArray(30); // Initialize an array that contains numbers from 1 to 30.
+    int[] inBuf = initArray(28, new Scanner(System.in)); // Get all user input in an array
+
+
+    // TODO: Program doesn't work when input = {1, 2, 3, 4, 5, ...., 27, 28}
+
+    sort(inBuf);
+
+    for (int i = 0; i < inBuf.length; i++) {
+      boolean isContained = search(arr[i], inBuf);
+      if (!isContained) {
+        System.out.println(arr[i]);
+      }
+    }
   }
 }
