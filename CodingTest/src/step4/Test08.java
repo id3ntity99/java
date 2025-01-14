f4325be01d678c1a417b6e4c5d62ba5ca406608e
id3ntity99 <jas06113@gmail.com>
2025-01-14 16:24:39 +0900

    Solved test8

--- CodingTest/src/step4/Test08.java
+++ CodingTest/src/step4/Test08.java
@@ -6,18 +6,35 @@ public class Test08 {
   public static void main(String[] args) {
     int[] buf = new int[10];
     Scanner sc = new Scanner(System.in);
+
     for (int i = 0; i < buf.length; i++) {
       int operand = sc.nextInt();
       buf[i] = operand % 42;
     }
 
-    int cnt = 0;
-    int key = buf[0];
-    for (int j = 1; j < buf.length; j++) {
-      if (key != buf[j]) {
-        cnt++;
+    sc.close();
+    // Sort the buffer
+    for (int i = 1; i < buf.length; i++) {
+      int key = buf[i];
+      int j = i - 1;
+      while (j >= 0 && buf[j] > key) {
+        int tmp = buf[j];
+        buf[j] = key;
+        buf[j + 1] = tmp;
+        j--;
       }
     }
 
+
+    // count duplicated elements from the buf
+    int[] temp = new int[buf.length];
+    int counter = 0;
+    for (int j = 0; j < buf.length - 1; j++) {
+      if (buf[j] == buf[j + 1]) {
+        counter++;
+      }
+    }
+
+    System.out.println(buf.length - counter);
   }
 }
