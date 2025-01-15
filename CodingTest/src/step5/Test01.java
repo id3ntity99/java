7af9d9f0d796d07b4f260776fcced16166dab55a
id3ntity99 <jas06113@gmail.com>
2025-01-15 17:37:10 +0900

    solved test1; BOJ 27866

--- CodingTest/src/step5/Test01.java
+++ CodingTest/src/step5/Test01.java
@@ -1,8 +1,18 @@
 package step5;
 
+import java.util.Scanner;
+
 /*-
  * BOJ 27866
  */
 public class Test01 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    int index = sc.nextInt() - 1;
+    sc.close();
 
+    char ch = str.charAt(index);
+    System.out.println(ch);
+  }
 }
