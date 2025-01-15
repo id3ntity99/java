0489bca06e141bee24e1765a4a89b31c7d26fced
id3ntity99 <jas06113@gmail.com>
2025-01-15 17:37:33 +0900

    Solved test2; BOJ 2743

---
+++ CodingTest/src/step5/Test02.java
@@ -0,0 +1,15 @@
+package step5;
+
+import java.util.Scanner;
+
+/*-
+ * BOJ 2743
+ */
+public class Test02 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    sc.close();
+    System.out.println(str.length());
+  }
+}
