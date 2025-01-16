4f96e16d053fde002e06351aaf6addf54fc966a6
id3ntity99 <jas06113@gmail.com>
2025-01-16 17:38:07 +0900

    Solving test03; BOJ 2444

---
+++ CodingTest/src/step6/Test03.java
@@ -0,0 +1,22 @@
+package step6;
+
+import java.util.Scanner;
+
+/*-
+ * BOJ 2444
+ */
+public class Test03 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+
+    for (int i = 0; i < n; i++) {
+      // TODO: print spaces (n - i - 1) times, and print asterisks (2(i+1) - 1) times
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+      // TODO: print pyramid in reverse order
+    }
+  }
+}
