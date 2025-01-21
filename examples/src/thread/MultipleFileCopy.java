5031301493d523abf87b703d806e0aed7f61fb67
Jason Lee <jas06113@gmail.com>
2025-01-22 00:53:47 +0900

    runnable that copies source file into destination

---
+++ examples/src/thread/MultipleFileCopy.java
@@ -0,0 +1,33 @@
+package thread;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Logger;
+
+// TODO Refactoring required for MultipleFileCopy and SingleFileCopy
+public class MultipleFileCopy implements Runnable {
+  private final InputStream in;
+  private final OutputStream out;
+  private static final int SIZE = 512;
+  private static final Logger LOGGER = Logger.getLogger(Thread.currentThread().getName());
+
+  public MultipleFileCopy(InputStream in, OutputStream out) {
+    this.in = in;
+    this.out = out;
+  }
+
+  @Override
+  public void run() {
+    try {
+      while (in.available() > 0) {
+        byte[] bytes = in.readNBytes(SIZE);
+        out.write(bytes);
+        out.flush();
+      }
+    } catch (IOException e) {
+      LOGGER.severe(e.getMessage());
+      Thread.currentThread().interrupt();
+    }
+  }
+}
