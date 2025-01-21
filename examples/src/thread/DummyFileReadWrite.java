e4fdb2cffcae88304e2daf6eeacd4b5c3d32a039
id3ntity99 <jas06113@gmail.com>
2025-01-21 12:34:51 +0900

    Changed implementation

--- examples/src/thread/DummyFileReadWrite.java
+++ examples/src/thread/DummyFileReadWrite.java
@@ -1,43 +1,30 @@
 package thread;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.logging.Logger;
 
-public class DummyFileReadWrite implements Closeable, Runnable {
+public class DummyFileReadWrite implements Runnable {
   private final InputStream in;
   private final OutputStream out;
   private static final Logger LOGGER = Logger.getLogger(Thread.currentThread().getName());
+  private static final int SIZE = 512;
 
   public DummyFileReadWrite(InputStream in, OutputStream out) {
     this.in = in;
     this.out = out;
   }
 
-  /*
-   * TODO: How to run while loop in multi-threaded environment?
-   */
   @Override
   public void run() {
-    String message = String.format("%s is copying the srouce file into the destination.",
-        Thread.currentThread().getName());
-    LOGGER.info(message);
     try {
-      while (in.available() > 0) {
-        byte[] bytes = in.readNBytes(512);
-        out.write(bytes);
-        out.flush();
-      }
+      byte[] bytes = in.readNBytes(SIZE);
+      out.write(bytes);
+      out.flush();
     } catch (IOException e) {
       e.printStackTrace();
+      Thread.currentThread().interrupt();
     }
-  }
-
-  @Override
-  public void close() throws IOException {
-    in.close();
-    out.close();
   }
 }
