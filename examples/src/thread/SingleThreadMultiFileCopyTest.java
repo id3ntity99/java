1f0692fec3bd0365646fe4a5ce8ae9948d13ac12
id3ntity99 <jas06113@gmail.com>
2025-01-24 17:38:10 +0900

    Copying multiple files in single threaded way

---
+++ examples/src/thread/SingleThreadMultiFileCopyTest.java
@@ -0,0 +1,56 @@
+package thread;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class SingleThreadMultiFileCopyTest {
+  private static final Logger LOGGER =
+      Logger.getLogger(SingleThreadMultiFileCopyTest.class.getName());
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String sourcesPath = InputHelper.getInput("Path to sources.txt: ", sc);
+    String dest = InputHelper.getInput("Destination path: ", sc);
+
+    try {
+      List<String> sources = InputHelper.getSources(sourcesPath);
+
+      Instant start = Instant.now();
+      int i = 1;
+      for (String source : sources) {
+        InputStream in = new FileInputStream(source);
+        OutputStream out = new FileOutputStream(dest + "copy" + i++ + ".txt");
+        AbstractFileCopy task = new MultipleFileCopy(in, out);
+
+        task.run();
+
+        in.close();
+        out.close();
+      }
+
+      Instant finish = Instant.now();
+      long timeElapsed = Duration.between(start, finish).toSeconds();
+      String msg = String.format("Time Elapsed: %d", timeElapsed);
+      LOGGER.info(msg);
+
+    } catch (FileNotFoundException e) {
+      String msg = String.format("File not found at %s", sourcesPath);
+      LOGGER.severe(msg);
+      e.printStackTrace();
+    } catch (IOException e) {
+      LOGGER.severe("IOExceiption raised");
+      e.printStackTrace();
+    }
+
+
+  }
+}
