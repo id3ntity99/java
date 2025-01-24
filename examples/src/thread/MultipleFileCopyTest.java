dc0cc47d1c38c0898461d5a954386090f13c6b86
id3ntity99 <jas06113@gmail.com>
2025-01-24 13:26:51 +0900

    Using InputHelper for processing user's input

--- examples/src/thread/MultipleFileCopyTest.java
+++ examples/src/thread/MultipleFileCopyTest.java
@@ -5,12 +5,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
-// TODO Measure performance of copying multiple files with multiple threads.
 public class MultipleFileCopyTest {
   private static final Logger LOGGER = Logger.getLogger(MultipleFileCopyTest.class.getName());
 
@@ -18,27 +19,32 @@ public class MultipleFileCopyTest {
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Path to sources.txt: ");
-    String sourcesPath = MultipleFileCopy.getSourcesPath(sc);
-
-    List<String> paths = MultipleFileCopy.getSources(sourcesPath);
-
+    String sourcesPath = InputHelper.getPathInput(sc);
     System.out.print("Destination path: ");
-    String dest = MultipleFileCopy.getDestination(sc);
+    String dest = InputHelper.getPathInput(sc);
+
+    List<String> paths = InputHelper.getSources(sourcesPath);
 
     // Iterate the paths array and spawn new thread per path which copies source file into
     // destination
     Iterator<String> iter = paths.iterator();
     int i = 1;
+    Instant start = Instant.now();
+
     while (iter.hasNext()) {
       String path = iter.next();
       InputStream in = new FileInputStream(path);
-      OutputStream out = new FileOutputStream(dest + "\\copy" + i++ + ".txt");
+      OutputStream out = new FileOutputStream(dest + "copy" + i++ + ".txt");
       Runnable copy = new MultipleFileCopy(in, out);
       Thread thread = new Thread(copy);
       thread.start();
       thread.join();
     }
 
+    Instant finish = Instant.now();
+    long timeElapsed = Duration.between(start, finish).toSeconds();
+    String msg = String.format("Time Elapsed: %d seconds", timeElapsed);
+    LOGGER.info(msg);
     sc.close();
   }
 }
