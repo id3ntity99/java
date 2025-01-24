ead0a9221958c868b04db2aed1e3e98f162c84ac
id3ntity99 <jas06113@gmail.com>
2025-01-24 13:39:21 +0900

    Measures time elapsed

--- examples/src/thread/SingleFileCopyTest.java
+++ examples/src/thread/SingleFileCopyTest.java
@@ -3,14 +3,19 @@ package thread;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
-// TODO: Measure execution time
 public class SingleFileCopyTest {
+  private static final Logger LOGGER = Logger.getLogger(SingleFileCopyTest.class.getName());
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String src = InputHelper.getInput(sc);
-    String dest = InputHelper.getInput(sc);
+
+    String src = InputHelper.getInput("Source file path: ", sc);
+    String dest = InputHelper.getInput("Destination Path: ", sc);
 
     try (FileInputStream in = new FileInputStream(src);
         FileOutputStream out = new FileOutputStream(dest)) {
@@ -23,11 +28,18 @@ public class SingleFileCopyTest {
       thread2.start();
       thread3.start();
 
+      Instant start = Instant.now();
+
       while (in.available() > 0) {// Concurrently copy single target-file.
         thread1.run();
         thread2.run();
         thread3.run();
       }
+
+      Instant finish = Instant.now();
+      long timeElapsed = Duration.between(start, finish).toSeconds();
+      String msg = String.format("Time Elapsed: %d seconds", timeElapsed);
+      LOGGER.info(msg);
 
       thread1.join();
       thread2.join();
