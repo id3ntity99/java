49b5f031b1d5fa29588cefe0b925d082543f759f
id3ntity99 <jas06113@gmail.com>
2025-01-24 12:45:48 +0900

    Abstract class that implements Runnable interface

---
+++ examples/src/thread/AbstractFileCopy.java
@@ -0,0 +1,15 @@
+package thread;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public abstract class AbstractFileCopy implements Runnable {
+  protected InputStream in;
+  protected OutputStream out;
+  protected static final int SIZE = 512;
+
+  protected AbstractFileCopy(InputStream in, OutputStream out) {
+    this.in = in;
+    this.out = out;
+  }
+}
