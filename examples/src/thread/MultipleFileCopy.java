679e20e93510a75c27f42740e85d98b5860248d3
id3ntity99 <jas06113@gmail.com>
2025-01-24 12:14:10 +0900

    refactored MultipleFileCopyTest's static method into MultipleFileCopy

--- examples/src/thread/MultipleFileCopy.java
+++ examples/src/thread/MultipleFileCopy.java
@@ -1,8 +1,15 @@
 package thread;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 // TODO Refactoring required for MultipleFileCopy and SingleFileCopy
@@ -15,6 +22,37 @@ public class MultipleFileCopy implements Runnable {
   public MultipleFileCopy(InputStream in, OutputStream out) {
     this.in = in;
     this.out = out;
+  }
+
+  // Retrieve paths of source files from a file named "sources.txt", which contains the paths
+  // of source files and is located at "resources" directory
+  public static List<String> getSources(String pathsFile) {
+    List<String> paths = new ArrayList<>();
+
+    try (InputStreamReader inReader = new InputStreamReader(new FileInputStream(pathsFile));
+        BufferedReader reader = new BufferedReader(inReader)) {
+      while (reader.ready()) {
+        String path = reader.readLine().trim();
+        paths.add(path);
+      }
+    } catch (FileNotFoundException e) {
+      String msg = String.format("Exception Raised: %s%nFile not found for %s...%n", e.getMessage(),
+          pathsFile);
+      LOGGER.severe(msg);
+      e.printStackTrace();
+    } catch (IOException e) {
+      LOGGER.severe("IOException Raised...");
+      e.printStackTrace();
+    }
+    return paths;
+  }
+
+  public static String getSourcesPath(Scanner sc) {
+    return sc.nextLine().trim();
+  }
+
+  public static String getDestination(Scanner sc) {
+    return sc.nextLine().trim();
   }
 
   @Override
--- examples/src/thread/MultipleFileCopyTest.java
+++ examples/src/thread/MultipleFileCopyTest.java
@@ -1,14 +1,10 @@
 package thread;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
@@ -16,46 +12,18 @@ import java.util.logging.Logger;
 
 // TODO Measure performance of copying multiple files with multiple threads.
 public class MultipleFileCopyTest {
-  private static Logger LOGGER = Logger.getLogger(MultipleFileCopyTest.class.getName());
+  private static final Logger LOGGER = Logger.getLogger(MultipleFileCopyTest.class.getName());
 
-  // Retrieve paths of source files from a file named "sources.txt", which contains the paths
-  // of source files and is located at "resources" directory
-  private static List<String> getSources(String pathsFile) {
-    List<String> paths = new ArrayList<>();
-
-    try (InputStreamReader inReader = new InputStreamReader(new FileInputStream(pathsFile));
-        BufferedReader reader = new BufferedReader(inReader)) {
-      while (reader.ready()) {
-        String path = reader.readLine().trim();
-        paths.add(path);
-      }
-    } catch (FileNotFoundException e) {
-      String msg = String.format("Exception Raised: %s%nFile not found for %s...%n", e.getMessage(),
-          pathsFile);
-      LOGGER.warning(msg);
-      e.printStackTrace();
-    } catch (IOException e) {
-      LOGGER.severe("IOException Raised...");
-      e.printStackTrace();
-    }
-    return paths;
-  }
-
-  private static String getDestination(Scanner sc) {
-    System.out.print("Destination path: ");
-    String dest = sc.nextLine();
-    return dest;
-  }
-
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws InterruptedException, IOException {
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Path to sources.txt: ");
-    String sourcesPath = sc.nextLine().trim();
+    String sourcesPath = MultipleFileCopy.getSourcesPath(sc);
 
-    List<String> paths = getSources(sourcesPath);
+    List<String> paths = MultipleFileCopy.getSources(sourcesPath);
 
-    String dest = getDestination(sc);
+    System.out.print("Destination path: ");
+    String dest = MultipleFileCopy.getDestination(sc);
 
     // Iterate the paths array and spawn new thread per path which copies source file into
     // destination
