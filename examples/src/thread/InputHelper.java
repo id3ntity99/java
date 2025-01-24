c0772692b24a5788b369a6a024b961ef66fbb62c
id3ntity99 <jas06113@gmail.com>
2025-01-24 13:21:54 +0900

    Helper class that processes user input

---
+++ examples/src/thread/InputHelper.java
@@ -0,0 +1,30 @@
+package thread;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputHelper {
+  // Retrieve paths of source files from a file named "sources.txt", which contains the paths
+  // of source files and is located at "resources" directory
+  public static List<String> getSources(String pathsFile) throws IOException {
+    List<String> paths = new ArrayList<>();
+
+    InputStreamReader inReader = new InputStreamReader(new FileInputStream(pathsFile));
+    BufferedReader reader = new BufferedReader(inReader);
+    while (reader.ready()) {
+      String path = reader.readLine().trim();
+      paths.add(path);
+    }
+    reader.close();
+    return paths;
+  }
+
+  public static String getPathInput(Scanner sc) {
+    return sc.nextLine().trim();
+  }
+}
