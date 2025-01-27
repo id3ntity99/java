b6a09b850af8ea2f65aad38d13cffcd93608993a
id3ntity99 <jas06113@gmail.com>
2025-01-27 16:08:09 +0900

    Implemented CRUD App Main class

--- CRUDTest/src/user1/AppMain.java
+++ CRUDTest/src/user1/AppMain.java
@@ -1,5 +1,6 @@
 package user1;
 
+import java.util.List;
 import java.util.Scanner;
 
 /*-
@@ -23,24 +24,33 @@ public class AppMain {
     System.out.print("Age: ");
     int age = sc.nextInt();
 
-    User user = new User(uid, name, phoneNum, age);
-    return user;
+    return new User(uid, name, phoneNum, age);
   }
 
-  public static void showUser() {
-
+  public static String findUser() {
+    System.out.print("ID: ");
+    return sc.next();
   }
 
-  public static void findUser() {
+  public static User modifyUser() {
+    System.out.print("New ID: ");
+    String uid = sc.next();
 
+    System.out.print("New Name: ");
+    String name = sc.next();
+
+    System.out.print("New Phone Number: ");
+    String phoneNum = sc.next();
+
+    System.out.print("New Age: ");
+    int age = sc.nextInt();
+
+    return new User(uid, name, phoneNum, age);
   }
 
-  public static void modifyUser() {
-
-  }
-
-  public static void removeUser() {
-
+  public static String removeUser() {
+    System.out.print("ID for remove: ");
+    return sc.next();
   }
 
   public static void main(String[] args) {
@@ -50,7 +60,7 @@ public class AppMain {
 
     boolean isRunning = true;
 
-    UserDao dao = UserDao.getInstance();
+    UserDAO dao = UserDAO.getInstance();
 
     while (isRunning) {
       System.out.println("0: 종료, 1: 입력, 2: 조회, 3: 검색, 4: 수정, 5: 삭제");
@@ -67,13 +77,30 @@ public class AppMain {
           dao.insertUser(user);
           break;
         case 2:
+          List<User> users = dao.selectUsers();
+          for (User u : users)
+            System.out.println(u);
           break;
         case 3:
+          String uid = findUser();
+          user = dao.selectUser(uid);
+          if (user != null) {
+            System.out.println(user);
+          } else
+            System.out.println("No such user exists");
           break;
         case 4:
+          user = modifyUser();
+          dao.updateUser(user);
+          System.out.println("Modification complete");
           break;
         case 5:
+          String removeUid = removeUser();
+          dao.deleteUser(removeUid);
+          System.out.printf("Removed %s%n", removeUid);
           break;
+        default:
+          System.out.printf("Illegal option %d, try again%n", answer);
       }
     }
     System.out.println("프로그램 종료...");
