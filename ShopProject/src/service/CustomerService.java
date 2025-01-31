126b3226e8e4bca30d5237aeda80970fee7381ed
id3ntity99 <jas06113@gmail.com>
2025-01-31 12:35:30 +0900

    Imlementing services

--- ShopProject/src/service/CustomerService.java
+++ ShopProject/src/service/CustomerService.java
@@ -1,5 +1,35 @@
 package service;
 
-public class CustomerService {
+import dao.CustomerDAO;
+import entity.Customer;
 
+public class CustomerService {
+  private static final CustomerService INSTANCE = new CustomerService();
+
+  public static CustomerService getInstnace() {
+    return INSTANCE;
+  }
+
+  private CustomerDAO dao = CustomerDAO.getInstance();
+
+  private CustomerService() {
+    // Empty constructor
+  }
+
+  // TODO Make this method return boolean value that tells successful-ness or throw exception
+  public void signUp(Customer customer) {
+    dao.insertCustomer(customer);
+  }
+
+  public Customer login(String custId) {
+    return dao.selectCustomer(custId);
+  }
+
+  public void removeCustomer(String custId) {
+    dao.deleteCustomer(custId);
+  }
+
+  public void modifyCustomer(Customer customer) {
+    dao.updateCustomer(customer);
+  }
 }
--- ShopProject/src/service/ProductService.java
+++ ShopProject/src/service/ProductService.java
@@ -1,5 +1,27 @@
 package service;
 
-public class ProductService {
+import java.sql.SQLException;
+import java.util.List;
+import dao.ProductDAO;
+import dao.internal.NoSuchProductException;
+import entity.Product;
 
+public class ProductService {
+  private final ProductDAO dao = ProductDAO.getInstance();
+
+  public ProductService() {
+    // Empty constructor
+  }
+
+  public void register(Product product) throws SQLException {
+    dao.insertProduct(product);
+  }
+
+  public List<Product> getProducts() throws SQLException {
+    return dao.selectProducts();
+  }
+
+  public Product search(int prodNo) throws SQLException, NoSuchProductException {
+    return dao.selectProduct(prodNo);
+  }
 }
