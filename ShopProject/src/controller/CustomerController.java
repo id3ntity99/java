1e96af5bfffef6bc4ff6485236fa9e2b669b65bb
id3ntity99 <jas06113@gmail.com>
2025-01-31 12:34:47 +0900

    Implementing controllers

--- ShopProject/src/controller/CustomerController.java
+++ ShopProject/src/controller/CustomerController.java
@@ -1,28 +1,59 @@
 package controller;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import entity.Customer;
+import service.CustomerService;
 
 public class CustomerController {
+  private static final Logger LOGGER = Logger.getLogger(CustomerController.class.getName());
+  private CustomerService customerService = CustomerService.getInstnace();
+
   public CustomerController() {
     // Empty constructor
   }
 
-  public void manage(Scanner sc) {
+  public void manage(Scanner sc) { // TODO Refactor this method
+    Customer logedInCustomer = null;
     while (true) {
       System.out.println("=============Customer Management=============");
-      System.out.println("| 0: 돌아가기 | 1: 회원가입 | 2: 회원목록 | 3: 로그인 |");
+      if (logedInCustomer != null) {
+        System.out.println("| 0: 돌아가기 | 1: 회원가입 | 2: 로그아웃 | 3: 회원목록 |");
+      } else {
+        System.out.println("| 0: 돌아가기 | 1: 회원가입 | 2: 로그인 | 3: 회원목록 |");
+      }
       System.out.print("선택 >> ");
 
       int choice = sc.nextInt();
 
       switch (choice) {
         case 0: {
-          break;
+          return;
         }
         case 1: {
+          Customer newCustomer = inputCustomer(sc);
+          customerService.signUp(newCustomer);
+          String msg = String.format("[회원가입 완료]  %s", newCustomer);
+          LOGGER.info(msg);
           break;
         }
         case 2: {
+          if (logedInCustomer == null) {
+            String custId = inputCustId(sc);
+            logedInCustomer = customerService.login(custId);
+            if (logedInCustomer == null) {
+              String msg = String.format("[로그인 실퍠] %s가 존재하지 않음", custId);
+              LOGGER.log(Level.WARNING, msg);
+            } else {
+              String msg = String.format("[로그인 성공] %s", logedInCustomer);
+              LOGGER.info(msg);
+            }
+          } else {
+            String msg = String.format("[로그아웃] %s가 정상적으로 로그아웃 됨", logedInCustomer);
+            LOGGER.info(msg);
+            logedInCustomer = null;
+          }
           break;
         }
         case 3: {
@@ -35,4 +66,28 @@ public class CustomerController {
     }
   }
 
+  public Customer inputCustomer(Scanner sc) {
+    sc.nextLine();
+
+    System.out.print("아이디: ");
+    String custId = sc.nextLine();
+
+    System.out.print("이름: ");
+    String name = sc.nextLine();
+
+    System.out.print("휴대전화: ");
+    String hp = sc.nextLine();
+
+    System.out.print("주소: ");
+    String addr = sc.nextLine();
+
+    return new Customer(custId, name, hp, addr);
+  }
+
+  public String inputCustId(Scanner sc) {
+    sc.nextLine();
+
+    System.out.print("아이디: ");
+    return sc.nextLine();
+  }
 }
--- ShopProject/src/controller/ProductController.java
+++ ShopProject/src/controller/ProductController.java
@@ -1,8 +1,54 @@
 package controller;
 
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Logger;
+import dao.internal.NoSuchProductException;
+import entity.Product;
+import service.ProductService;
 
 public class ProductController {
+  private static final Logger LOGGER = Logger.getLogger(ProductController.class.getName());
+  private final ProductService service = new ProductService();
+
+  private Product getProductInput(Scanner sc) {
+    sc.nextLine();
+
+    System.out.print("제품 번호: ");
+    int prodNo = sc.nextInt();
+
+    sc.nextLine();
+
+    System.out.print("제품 이름: ");
+    String prodName = sc.nextLine();
+
+
+    System.out.print("수량: ");
+    int stock = sc.nextInt();
+
+    System.out.print("가격: ");
+    int price = sc.nextInt();
+
+    sc.nextLine();
+
+    System.out.print("제조 회사: ");
+    String company = sc.nextLine();
+
+    return new Product(prodNo, prodName, stock, price, company);
+  }
+
+  private int getProductNumber(Scanner sc) {
+    System.out.print("제품 번호: ");
+    return sc.nextInt();
+  }
+
+  private void handleException(Exception e) {
+    String msg = String.format("[예외 발생] %s%n[Stack Trace] %s", e.getMessage(),
+        Arrays.toString(e.getStackTrace()));
+    LOGGER.severe(msg);
+  }
 
   public void manage(Scanner sc) {
     while (true) {
@@ -14,15 +60,46 @@ public class ProductController {
 
       switch (choice) {
         case 0: {
-          break;
+          return;
         }
         case 1: {
+          Product newProduct = getProductInput(sc);
+          try {
+            service.register(newProduct);
+          } catch (SQLException e) {
+            this.handleException(e);
+          }
+          String msg = String.format("[상품 등록 완료] %s", newProduct);
+          LOGGER.info(msg);
           break;
         }
         case 2: {
+          try {
+            List<Product> prods = service.getProducts();
+            for (Product prod : prods) {
+              String output =
+                  String.format("제품 번호: %d%n제품 이름: %s%n수량: %d%n가격: %d%n제조사: %s%n", prod.getProdNo(),
+                      prod.getProdName(), prod.getStock(), prod.getPrice(), prod.getCompany());
+              System.out.println(output);
+            }
+          } catch (SQLException e) {
+            this.handleException(e);
+          }
           break;
         }
         case 3: {
+          int prodNo = getProductNumber(sc);
+          try {
+            Product prod = service.search(prodNo);
+            String msg =
+                String.format("제품 번호: %d%n제품 이름: %s%n수량: %d%n가격: %d%n제조사: %s%n", prod.getProdNo(),
+                    prod.getProdName(), prod.getStock(), prod.getPrice(), prod.getCompany());
+            System.out.println(msg);
+          } catch (SQLException e) {
+            handleException(e);
+          } catch (NoSuchProductException e) {
+            LOGGER.warning(e.getMessage());
+          }
           break;
         }
         default: {
