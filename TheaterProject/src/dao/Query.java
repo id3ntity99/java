83e27cbd3bc20ae3d3e96109d3c76ead9286369a
Jason Lee <jas06113@gmail.com>
2025-02-01 23:09:46 +0900

    Added select where id query

--- TheaterProject/src/dao/Query.java
+++ TheaterProject/src/dao/Query.java
@@ -1,13 +1,14 @@
 package dao;
 
 public final class Query {
-  public static final String SELECT_MOVIE = "SELECT * FROM `movies` WHERE `movie_id` = ?;";
+  public static final String SELECT_MOVIE_WITH_TITLE = "SELECT * FROM `movies` WHERE `title` = ?;";
+  public static final String SELECT_MOVIE_WITH_ID = "SELECT * FROM `movies` WHERE `movie_id`=?;";
   public static final String SELECT_MOVIES = "SELECT * FROM `movies`;";
 
   public static final String INSERT_MOVIE = "INSERT INTO `movies` VALUES (?, ?, ?, NOW());";
 
   public static final String UPDATE_MOVIE =
-      "UPDATE `movies` SET `movie_id`=?, `title`=?, `genre`=?, `release_date`=?;";
+      "UPDATE `movies` SET `title`=?, `genre`=?, `release_date`=? WHERE `movie_id`=?";
 
   public static final String DELETE_MOVIE = "DELETE FROM `movies` WHERE `movie_id`=?;";
 
