f03ca5284ab02d652d3ac70ff8b4d799cf97ae11
Jason Lee <jas06113@gmail.com>
2025-02-03 00:33:27 +0900

    Implementing main

--- TheaterProject/src/main/AppMain.java
+++ TheaterProject/src/main/AppMain.java
@@ -1,7 +1,52 @@
 package main;
 
-public class AppMain {
-  public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.logging.Logger;
+import controller.MoviesController;
+import internal.Prompt;
 
+public class AppMain {
+  private static final Logger LOGGER = Logger.getLogger(AppMain.class.getName());
+
+  public AppMain() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public static void main(String[] args) {
+    String[] options = {"Exit", "Movies", "Customers", "Booking"};
+
+    Prompt prom = new Prompt("Theater Management System", options, ">>");
+    Scanner sc = new Scanner(System.in);
+
+    boolean isRunning = true;
+
+    prom.printTitle();
+    while (isRunning) {
+      prom.printOptions();
+      prom.printInputSymbol();
+      int choice = sc.nextInt();
+
+      switch (choice) {
+        case 1: {
+          LOGGER.info("Terminating system...");
+          isRunning = false;
+          break;
+        }
+        case 2: {
+          MoviesController.getInstance().run(sc);
+          break;
+        }
+        case 3: {
+          break;
+        }
+        case 4: {
+          break;
+        }
+        default:
+          LOGGER.info("");
+          break;
+      }
+    }
+    sc.close();
   }
 }
