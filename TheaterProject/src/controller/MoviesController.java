607061d167ff1b8d41b7a0d1d1abb952c72e1f31
id3ntity99 <jas06113@gmail.com>
2025-02-03 09:03:54 +0900

    Implementing update and list movies

--- TheaterProject/src/controller/MoviesController.java
+++ TheaterProject/src/controller/MoviesController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
 import entity.Movie;
@@ -15,22 +16,28 @@ public class MoviesController {
     // Empty constructor
   }
 
-  public Movie getNewMovie(Scanner sc) {
+  private void print(Movie movie) {
+    String msg = String.format("Movie ID: %d%nTitle:%s%nGenre:%s%nRelease Date:%s%n",
+        movie.getMovieId(), movie.getTitle(), movie.getGenre(), movie.getReleaseDate());
+    System.out.println(msg);
+  }
+
+  private Movie getNewMovie(Prompt prompt, Scanner sc) {
     Movie movie = new Movie();
 
-    System.out.print("Movie ID: ");
+    prompt.ask("Movie ID");
     int movieId = sc.nextInt();
     movie.setMovieId(movieId);
 
-    System.out.print("Title: ");
+    prompt.ask("Title: ");
     String title = sc.nextLine();
     movie.setTitle(title);
 
-    System.out.print("Genre: ");
+    prompt.ask("Genre: ");
     String genre = sc.nextLine();
     movie.setGenre(genre);
 
-    System.out.print("Release Date");
+    prompt.ask("Release Date");
     String releaseDate = sc.nextLine();
     movie.setReleaseDate(releaseDate);
 
@@ -55,8 +62,9 @@ public class MoviesController {
         case 1: {// Go back to AppMain
           return;
         }
+
         case 2: {
-          Movie movie = getNewMovie(sc);
+          Movie movie = getNewMovie(prompt, sc);
           try {
             service.enroll(movie);
           } catch (SQLException e) {
@@ -65,23 +73,45 @@ public class MoviesController {
           }
           break;
         }
+
         case 3: {// Search movie with name
           String name = sc.nextLine();
           try {
             Movie movie = service.search(name);
-            String msg = String.format("Movie ID: %d%nTitle:%s%nGenre:%s%nRelease Date:%s%n",
-                movie.getMovieId(), movie.getTitle(), movie.getGenre(), movie.getReleaseDate());
-            System.out.println(msg);
+            print(movie);
           } catch (SQLException e) {
             String msg = String.format("[Search Failed] %s", e.getMessage());
             LOGGER.severe(msg);
           }
           break;
         }
+
         case 4: {// Search movie with ID
+          String movieId = sc.nextLine();
+          try {
+            Movie movie = service.search(movieId);
+            print(movie);
+          } catch (SQLException e) {
+            String msg = String.format("[Search Failed] %s", e.getMessage());
+            LOGGER.severe(msg);
+          }
           break;
         }
-        case 5: {
+
+        case 5: {// List all movies
+          try {
+            List<Movie> movies = service.getMovies();
+          } catch (SQLException e) {
+            String msg = String.format("[List Failed] %s", e.getMessage());
+            LOGGER.severe(msg);
+          }
+        }
+
+        case 6: {// Update movie information
+          Movie movie = getNewMovie(prompt, sc);
+        }
+
+        case 7: {// Delete movie information from DB
 
         }
 
